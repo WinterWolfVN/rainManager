9666cf7770fa9785468ae8ffbae6d9f13a4bbd64
WinterWolfVN <winterwolfvn123@gmail.com>
2026-04-07 21:55:21 +0700

    Update PMInstaller.kt

--- app/src/main/kotlin/com/aliucord/manager/installers/pm/PMInstaller.kt
+++ app/src/main/kotlin/com/aliucord/manager/installers/pm/PMInstaller.kt
@@ -16,16 +16,12 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import java.io.File
 import kotlin.coroutines.resume
 
-/**
- * APK installer using the [PackageInstaller] from the system's [PackageManager] service.
- */
 class PMInstaller(
     private val context: Application,
 ) : Installer {
     private val _packageInstaller = context.packageManager.packageInstaller
 
     init {
-        // Destroy all open sessions that may have not been previously cleaned up due to fatal errors
         for (session in _packageInstaller.mySessions) {
             Log.d(BuildConfig.TAG, "Deleting old PackageInstaller session ${session.sessionId}")
             _packageInstaller.abandonSession(session.sessionId)
@@ -37,10 +33,8 @@ class PMInstaller(
     }
 
     override suspend fun waitInstall(apks: List<File>, silent: Boolean) = suspendCancellableCoroutine { continuation ->
-        // Create a new install session
         val sessionId = createInstallSession(silent)
 
-        // This will receive parsed data forwarded by PMIntentReceiver
         val relayReceiver = PMResultReceiver(
             sessionId = sessionId,
             isUninstall = false,
@@ -48,14 +42,12 @@ class PMInstaller(
         )
 
         ContextCompat.registerReceiver(
-            /* context = */ context,
-            /* receiver = */ relayReceiver,
-            /* filter = */ PMResultReceiver.intentFilter,
-            /* flags = */ ContextCompat.RECEIVER_NOT_EXPORTED,
+            context,
+            relayReceiver,
+            PMResultReceiver.intentFilter,
+            ContextCompat.RECEIVER_NOT_EXPORTED,
         )
 
-        // Unregister PMResultReceiver when this coroutine finishes or error occurs
-        // Additionally, cancel the install session entirely
         continuation.invokeOnCancellation { error ->
             context.unregisterReceiver(relayReceiver)
             context.packageManager.packageInstaller.abandonSession(sessionId)
@@ -69,14 +61,14 @@ class PMInstaller(
             .putExtra(PMIntentReceiver.EXTRA_SESSION_ID, -1)
             .putExtra(PMIntentReceiver.EXTRA_RELAY_ENABLED, true)
 
-        val pendingIntent = PendingIntent.getBroadcast(
-            /* context = */ context,
-            /* requestCode = */ 0,
-            /* intent = */ callbackIntent,
-            /* flags = */ PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
-        )
+        val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(context, 0, callbackIntent, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        } else {
+            PendingIntent.getBroadcast(context, 0, callbackIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
 
-        // This will receive parsed data forwarded by PMIntentReceiver
+        val pendingIntent = flags
+
         val relayReceiver = PMResultReceiver(
             sessionId = -1,
             isUninstall = true,
@@ -84,13 +76,12 @@ class PMInstaller(
         )
 
         ContextCompat.registerReceiver(
-            /* context = */ context,
-            /* receiver = */ relayReceiver,
-            /* filter = */ PMResultReceiver.intentFilter,
-            /* flags = */ ContextCompat.RECEIVER_NOT_EXPORTED,
+            context,
+            relayReceiver,
+            PMResultReceiver.intentFilter,
+            ContextCompat.RECEIVER_NOT_EXPORTED,
         )
 
-        // Unregister PMResultReceiver when this coroutine is cancelled early
         continuation.invokeOnCancellation { error ->
             context.unregisterReceiver(relayReceiver)
         }
@@ -98,11 +89,6 @@ class PMInstaller(
         _packageInstaller.uninstall(packageName, pendingIntent.intentSender)
     }
 
-    /**
-     * Starts a [PackageInstaller] session with the necessary params.
-     * @param silent If this is an update, then the update will occur without user interaction.
-     * @return The open install session id.
-     */
     private fun createInstallSession(silent: Boolean): Int {
         val params = SessionParams(SessionParams.MODE_FULL_INSTALL).apply {
             setInstallLocation(PackageInfo.INSTALL_LOCATION_AUTO)
@@ -112,8 +98,6 @@ class PMInstaller(
 
             if (Build.VERSION.SDK_INT >= 31) {
                 setInstallScenario(PackageManager.INSTALL_SCENARIO_FAST)
-
-                // Allegedly MIUI is not happy with silent installs
                 if (silent && !isMiui()) {
                     setRequireUserAction(SessionParams.USER_ACTION_NOT_REQUIRED)
                 }
@@ -125,26 +109,19 @@ class PMInstaller(
         return context.packageManager.packageInstaller.createSession(params)
     }
 
-    /**
-     * Start a [PackageInstaller] session for installation.
-     * @param apks The apks to install
-     * @param relay Whether to use the [PMResultReceiver] flow.
-     * @return The [PendingIntent] that was registered to the [PackageInstaller.Session]
-     */
     private fun startInstall(sessionId: Int, apks: List<File>, relay: Boolean) {
         val callbackIntent = Intent(context, PMIntentReceiver::class.java)
             .putExtra(PMIntentReceiver.EXTRA_SESSION_ID, sessionId)
             .putExtra(PMIntentReceiver.EXTRA_RELAY_ENABLED, relay)
 
-        val pendingIntent = PendingIntent.getBroadcast(
-            /* context = */ context,
-            /* requestCode = */ 0,
-            /* intent = */ callbackIntent,
-            /* flags = */ PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT,
-        )
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getBroadcast(context, 0, callbackIntent, PendingIntent.FLAG_MUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        } else {
+            PendingIntent.getBroadcast(context, 0, callbackIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
 
         _packageInstaller.openSession(sessionId).use { session ->
-            val bufferSize = 1 * 1024 * 1024 // 1MiB
+            val bufferSize = 2 * 1024 * 1024
 
             for (apk in apks) {
                 session.openWrite(apk.name, 0, apk.length()).use { outStream ->
